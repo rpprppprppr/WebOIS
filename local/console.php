4a1a9a18a6aab6bb4ff7163a0b720431cf854953
rpprppprppr <kondrenkodo@gmail.com>
2025-12-18 04:22:04 +0500

    install composer

--- local/console.php
+++ local/console.php
@@ -10,7 +10,7 @@ define("NO_KEEP_STATISTIC", true);
 define("NOT_CHECK_PERMISSIONS", true);
 
 $_SERVER['DOCUMENT_ROOT'] = DocumentRoot::get();
-require_once($_SERVER['DOCUMENT_ROOT'].'/bitrix/modules/main/include/prolog_before.php');
+require_once $_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/main/include/prolog_before.php';
 
 $app = new Application('Legacy Console App', 'v1.0.0');
 $app -> add(new UpdateCommand());
